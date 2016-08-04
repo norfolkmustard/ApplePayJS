ed48fbca462ae41042e25535615ca089e17fec22
norfolkmustard <github@norfolkmustard.com>
2016-08-04 19:01:07 +0100

    Update apple_pay_conf.php

--- apple_pay_conf.php
+++ apple_pay_conf.php
@@ -4,7 +4,8 @@
 define('PRODUCTION_CERTIFICATE_KEY', '/var/www/applepay_includes/ApplePay.key.pem'); 
 define('PRODUCTION_CERTIFICATE_PATH', '/var/www/applepay_includes/ApplePay.crt.pem');
 
-
+define('PRODUCTION_CURRENCYCODE', 'GBP');	//https://en.wikipedia.org/wiki/ISO_4217
+define('PRODUCTION_COUNTRYCODE', 'GB');		//https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
 define('PRODUCTION_MERCHANTIDENTIFIER', 'merchant.com.blurg.shop');
 define('PRODUCTION_DISPLAYNAME', 'My Test Shop');
 
