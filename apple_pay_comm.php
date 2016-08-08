445da6c5f8c0ab84415875921ba2c76136ddafba
norfolkmustard <github@norfolkmustard.com>
2016-08-08 21:30:00 +0100

    Update apple_pay_comm.php

--- apple_pay_comm.php
+++ apple_pay_comm.php
@@ -4,7 +4,7 @@ $validation_url = $_GET['u'];
 
 if( "https" == parse_url($validation_url, PHP_URL_SCHEME) && substr( parse_url($validation_url, PHP_URL_HOST), -9 )  == "apple.com" ){
 
-	require_once ('/var/www/applepay_includes/apple_pay_conf.php');
+	require_once ('/your/path/to/applepay_includes/apple_pay_conf.php');
 	
 	// create a new cURL resource
 	$ch = curl_init();
