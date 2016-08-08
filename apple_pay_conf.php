ec4dc5e4aa6d070e54d6c62689b76c2455d32066
norfolkmustard <github@norfolkmustard.com>
2016-08-08 21:29:28 +0100

    Update apple_pay_conf.php

--- apple_pay_conf.php
+++ apple_pay_conf.php
@@ -1,7 +1,7 @@
 <?php
 //update these with the real location of your two .pem files. keep them above/outside your webroot folder
-define('PRODUCTION_CERTIFICATE_KEY', '/var/www/applepay_includes/ApplePay.key.pem');
-define('PRODUCTION_CERTIFICATE_PATH', '/var/www/applepay_includes/ApplePay.crt.pem');
+define('PRODUCTION_CERTIFICATE_KEY', '/your/path/to/applepay_includes/ApplePay.key.pem');
+define('PRODUCTION_CERTIFICATE_PATH', '/your/path/to/applepay_includes/ApplePay.crt.pem');
 
 define('PRODUCTION_CURRENCYCODE', 'GBP');	//https://en.wikipedia.org/wiki/ISO_4217
 define('PRODUCTION_COUNTRYCODE', 'GB');		//https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
