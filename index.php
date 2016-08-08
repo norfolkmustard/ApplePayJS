3a45838e4cc31712627071baffb2ff7e481dfe2d
norfolkmustard <github@norfolkmustard.com>
2016-08-08 22:03:12 +0100

    NBs and resolve if debug == true
    added some NBs to the console logs, and changed default to success only if in debug

--- index.php
+++ index.php
@@ -27,6 +27,8 @@ require_once ('/your/path/to/applepay_includes/apple_pay_conf.php');
 </div>
 <script type="text/javascript">
 
+var debug = <?=DEBUG?>;
+
 if (window.ApplePaySession) {
    var merchantIdentifier = '<?=PRODUCTION_MERCHANTIDENTIFIER?>';
    var promise = ApplePaySession.canMakePaymentsWithActiveCard(merchantIdentifier);
@@ -133,6 +135,7 @@ document.getElementById("applePay").onclick = function(evt) {
 
 	session.onshippingcontactselected = function(event) {
 		logit('starting session.onshippingcontactselected');
+		logit('NB: At this stage, apple only reveals the Country, Locality and 4 characters of the PostCode to protect the privacy of what is only a *prospective* customer at this point. This is enough for you to determine shipping costs, but not the full address of the customer.');
 		logit(event);
 		
 		getShippingOptions( event.shippingContact.countryCode );
@@ -177,6 +180,7 @@ document.getElementById("applePay").onclick = function(evt) {
 	session.onpaymentauthorized = function (event) {
 
 		logit('starting session.onpaymentauthorized');
+		logit('NB: This is the first stage when you get the *full shipping address* of the customer, in the event.payment.shippingContact object');
 		logit(event);
 
 		var promise = sendPaymentToken(event.payment.token);
@@ -199,8 +203,10 @@ document.getElementById("applePay").onclick = function(evt) {
 			
 			logit("this is where you would pass the payment token to your third-party payment provider to use the token to charge the card. Only if your provider tells you the payment was successful should you return a resolve(true) here. Otherwise reject;");
 			logit("defaulting to resolve(true) here, just to show what a successfully completed transaction flow looks like");
+			if ( debug == true )
 			resolve(true);
-			//reject;
+			else
+			reject;
 		});
 	}
 	
@@ -214,7 +220,6 @@ document.getElementById("applePay").onclick = function(evt) {
 };
 	
 function logit( data ){
-	var debug = <?=DEBUG?>;
 	
 	if( debug == true ){
 		console.log(data);
