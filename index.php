7c82b39170033c8ce5057954ed5366a98de9b024
norfolkmustard <github@norfolkmustard.com>
2016-08-08 21:30:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once ('/var/www/applepay_includes/apple_pay_conf.php');
+require_once ('/your/path/to/applepay_includes/apple_pay_conf.php');
 ?>
 <!DOCTYPE html>
 <html lang="en-GB">
