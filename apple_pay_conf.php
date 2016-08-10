06aea83fc39d20a3a7d31a08593b086b70281423
Stu <github@norfolkmustard.com>
2016-08-10 20:56:57 +0100

    Update apple_pay_conf.php

--- apple_pay_conf.php
+++ apple_pay_conf.php
@@ -2,14 +2,14 @@
 //update these with the real location of your two .pem files. keep them above/outside your webroot folder
 define('PRODUCTION_CERTIFICATE_KEY', '/your/path/to/applepay_includes/ApplePay.key.pem');
 define('PRODUCTION_CERTIFICATE_PATH', '/your/path/to/applepay_includes/ApplePay.crt.pem');
-define('PRODUCTION_MERCHANTIDENTIFIER', openssl_x509_parse( file_get_contents( PRODUCTION_CERTIFICATE_PATH ))['subject']['UID'] );
+
+define('PRODUCTION_MERCHANTIDENTIFIER', openssl_x509_parse( file_get_contents( PRODUCTION_CERTIFICATE_PATH ))['subject']['UID'] ); //e.g. merchant.com.mydomain or merchant.com.mydomain.shop
+define('PRODUCTION_DOMAINNAME', $_SERVER["HTTP_HOST"]); //e.g. shop.mydomain.com or mydomain.com
 
 
 define('PRODUCTION_CURRENCYCODE', 'GBP');	//https://en.wikipedia.org/wiki/ISO_4217
 define('PRODUCTION_COUNTRYCODE', 'GB');		//https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
 define('PRODUCTION_DISPLAYNAME', 'My Test Shop');
-
-define('PRODUCTION_DOMAINNAME', $_SERVER["HTTP_HOST"]);
 
 define('DEBUG', 'true');
 ?>
