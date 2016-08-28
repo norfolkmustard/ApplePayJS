69c8d1c326b7a5a069043827e62fe45227d2fbf2
Stu <github@norfolkmustard.com>
2016-08-28 19:01:12 +0100

    Update curl_test.php

--- curl_test.php
+++ curl_test.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 $validation_url = $_GET['u'] ? $_GET['u'] : "https://apple-pay-gateway-cert.apple.com/paymentservices/startSession";
 
 
